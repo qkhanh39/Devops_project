4e47fb5163f1e7393dd3ac48d8720997bbfc84d4
qkhanh39 <khanhtruong3904@gmail.com>
2024-12-08 17:54:46 +0700

    Change things

--- server/database.py
+++ server/database.py
@@ -8,10 +8,10 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
-    notes = db.relationship('History')
+    images = db.relationship('Images')
 
-class History(db.Model):
+class Images(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    action = db.Column(db.String(150))
+    original_image = db.Column(db.String(150))
+    pixel_image = db.Column(db.String(150))
--- server/views.py
+++ server/views.py
@@ -5,11 +5,13 @@ from flask_login import login_required, current_user
 from werkzeug.utils import secure_filename
 import os
 import urllib.request
-
+from PIL import Image
 from .model import Pyx, Pal
-
+from datetime import datetime
 from skimage import io
 from PIL import Image
+from .database import Images
+from . import db
 
 views = Blueprint('views', __name__, template_folder='../client/templates')
 
@@ -18,6 +20,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 
 @views.route('/', methods=['GET', 'POST'])
@@ -32,54 +35,37 @@ def home():
             flash('No selected file', category='error')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            filename = f'{current_user.username}_{timestamp}.png' 
             file_path = os.path.join(
                 current_app.config['UPLOAD_FOLDER'], current_user.username, filename)
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
-            # file.save(file_path)
             image = Image.open(file)
-            max_size = (512, 512)  # Define the maximum size
+            max_size = (512, 512) 
             image.thumbnail(max_size)
             image.save(file_path)
-            flash(
-                f'File uploaded successfully', category='success')
-            render_template("home.html", user=current_user,
+            flash(f'File uploaded successfully', category='success')
+            render_template("home.html", 
+                            user=current_user,
                             filename=f'uploads/{current_user.username}/{filename}')
-            #run_transfer(file_path)
-            # , transfer_filename='transferImages/output.png')
-            return render_template("home.html", user=current_user, filename=f'uploads/{current_user.username}/{filename}')
+            width, height = Image.open(file_path).size
+            output_path, pixel_image_name = run_transfer(file_path, current_user.username)
+            new_image = Images(user_id = current_user.id, original_image=filename, pixel_image=pixel_image_name)
+            db.session.add(new_image)
+            db.session.commit()
+            return render_template("home.html", 
+                                   user=current_user, 
+                                   filename=f'uploads/{current_user.username}/{filename}', 
+                                   width=width, 
+                                   height=height,
+                                   output=output_path)
         else:
             flash('Allowed file types are png, jpg, jpeg', category='error')
             return redirect(request.url)
     return render_template("home.html", user=current_user)
 
 
-def plot(subplots=[], save_as=None, fig_h=9):
-    """Plotting helper function"""
-    fig, ax = plt.subplots(int(np.ceil(len(subplots) / 3)),
-                           min(3, len(subplots)),
-                           figsize=(18, fig_h))
-    if len(subplots) == 1:
-        ax = [ax]
-    else:
-        ax = ax.ravel()
-    for i, subplot in enumerate(subplots):
-        if isinstance(subplot, dict):
-            ax[i].set_title(subplot["title"])
-            ax[i].imshow(subplot["image"])
-        else:
-            ax[i].imshow(subplot)
-    fig.tight_layout()
-    plt.savefig(save_as)
-    plt.close(fig)
-
-
-def run_transfer(image_path):
-    # file_path = 'server/static/transferImages/output.png'
-    # image = Image.open(image_path)
-    # max_size = (512, 512)  # Define the maximum size
-    # image.thumbnail(max_size)
-    # image.save(file_path)
+def run_transfer(image_path, username):
     print(image_path)
     image = io.imread(image_path)
     downsample_by = 6
@@ -87,4 +73,13 @@ def run_transfer(image_path):
     pyx = Pyx(factor=downsample_by, palette=palette)
     pyx.fit(image)
     new_image = pyx.transform(image)
-    plot([new_image], "server/static/transferImages/output.png")
+    new_image = Image.fromarray(new_image)
+    output_folder = os.path.join(current_app.config['OUTPUT_FOLDER'], username)
+    os.makedirs(output_folder, exist_ok=True)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")  
+    filename = f'{username}_{timestamp}.png'  
+    output_path = os.path.join(output_folder, filename)
+    new_image.save(output_path)
+    relative_path = f'transferImages/{username}/{filename}'
+    return relative_path, filename
