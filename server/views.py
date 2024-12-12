af26893fbb8c6d6683bdf1ce7d37976db8e56135
qkhanh39 <khanhtruong3904@gmail.com>
2024-12-12 22:52:45 +0700

    test

--- server/views.py
+++ server/views.py
@@ -20,6 +20,8 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
