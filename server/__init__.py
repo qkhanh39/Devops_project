76ffeb4f024d054e3468cda2bfa0d97c126d4710
qkhanh39 <khanhtruong3904@gmail.com>
2024-12-16 10:57:07 +0700

    test

--- server/__init__.py
+++ server/__init__.py
@@ -8,6 +8,7 @@ db = SQLAlchemy()
 UPLOAD_FOLDER = 'server/static/uploads/'
 OUTPUT_FOLDER = 'server/static/transferImages/'
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret-key'
